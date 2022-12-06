4564f6de50829f7bb1918a22f4e86af4a08e5d4a
reykeem <106338511+reykeem@users.noreply.github.com>
2022-12-06 12:41:54 -0800

    configured server

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/index.html"));
+// });
 
 app.use(express.static(path.resolve(__dirname, "../client")));
 
